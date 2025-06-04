a4aaf655773904e533a495a9afd89024028fed01
saketh n <saketha.n@imtf.com>
2025-06-04 18:22:23 +0530

    added endpoint

--- src/main/java/com/example/kyc/resource/WsStatusResource.java
+++ src/main/java/com/example/kyc/resource/WsStatusResource.java
@@ -10,7 +10,7 @@ import java.util.logging.Logger;
 
 import jakarta.annotation.security.RolesAllowed;
 
-@Path("/kyc")
+@Path("api/kyc")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class WsStatusResource {
